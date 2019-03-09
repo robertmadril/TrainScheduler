e30b2c12dfea49e0e01541152c8810fe2adb3afb
robertmadril <robertmmadril@gmail.com>
2019-03-09 15:17:56 -0700

    add submit button on click event

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAsIvHrkeHhmRKfUmJ4M5_Yb0M4U1kj1es",
+    authDomain: "rmm-train-schedule.firebaseapp.com",
+    databaseURL: "https://rmm-train-schedule.firebaseio.com",
+    projectId: "rmm-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "296416708252"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  console.log("Work")
+
+  $("#submit-btn").on("click", function(event) {
+      event.preventDefault();
+
+      var trainInput = $("#train-input").val().trim();
+      var destinationInput = $("#destination-input").val().trim();
+      var firstTrainInput = $("#first-train-input").val().trim();
+      var frequencyInput = $("#frequency-input").val().trim();
+
+      console.log(trainInput)
+
+      var newTrain = {
+          name: trainInput,
+          destination: destinationInput,
+          firstTrain: firstTrainInput,
+          frequency: frequencyInput
+      }
+
+      database.ref().push(newTrain);
+  });
+
+  database.ref().on("child_added", function(snapshot){
+      
+  }, function(err){
+
+  })
+
+
+
