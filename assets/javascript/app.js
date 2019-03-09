820c377296a4948cc775bd8b403ffcaa1ac68589
robertmadril <robertmmadril@gmail.com>
2019-03-09 15:27:02 -0700

    add functionality to add child listen event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,44 +8,50 @@
     messagingSenderId: "296416708252"
   };
   firebase.initializeApp(config);
-
+  //hold database value for readability
   var database = firebase.database();
 
-  console.log("Work")
-
   $("#submit-btn").on("click", function(event) {
+      //prevents refresh
       event.preventDefault();
-
+        //hold input values
       var trainInput = $("#train-input").val().trim();
       var destinationInput = $("#destination-input").val().trim();
       var firstTrainInput = $("#first-train-input").val().trim();
       var frequencyInput = $("#frequency-input").val().trim();
-
-      console.log(trainInput)
-
+        //var to hold object that will be pushed to db
       var newTrain = {
           name: trainInput,
           destination: destinationInput,
           firstTrain: firstTrainInput,
           frequency: frequencyInput
       }
-
+      //push to firebase database
       database.ref().push(newTrain);
   });
 
   database.ref().on("child_added", function(snapshot){
-
+    //hold object value
     var sv = snapshot.val();
-
+    //direct storing of database vars
     var dbName = sv.name;
     var dbDestination = sv.destination;
     var dbFirstTrain = sv.firstTrain;
     var dbFrequency = sv.frequency;
-
+    //time manipulation
     
+    //create row element to append to html page
+    var newRow = $("<tr>");
+
+    newRow.append("<td>" + dbName + "</td>");
+    newRow.append("<td>" + dbDestination + "</td>");
+    //append row to html
+    $("#table-display").append(newRow);
+
 
   }, function(err){
-
+      //error message if database is not connecting
+    console.log("Oh no :(. This is the error: ")
   })
 
 
